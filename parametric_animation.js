2ede09afaa7bd08c614e2388fc155f1d76a2b8bd
Ryan Taylor <rythtayl@gmail.com>
2020-07-11 21:42:42 +0000

    added paraplot

--- parametric_animation.js
+++ parametric_animation.js
@@ -12,7 +12,7 @@ const gen_torus = gpu.createKernel(function(width, height, i, j,l) {
     if (width < height)
 	space = width/6;
     
-    var t = this.thread.x/50;
+    var t = this.thread.x/20;
 
     var x = space*((1+Math.pow(Math.sin(t/i+l),2))*cos(t)+cos(j*t+.75-l));
     var y = space*((1+Math.pow(Math.sin(t/i+l),2))*sin(t)+sin(j*t+.75+l));
@@ -22,31 +22,23 @@ const gen_torus = gpu.createKernel(function(width, height, i, j,l) {
     
     return 4*(Math.trunc(x)+Math.trunc(y)*width);
 })
-      .setOutput([10000])
+      .setOutput([15000])
 
 function frame() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.style.width ='100%';
+    canvas.style.height='100%';
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
     
     imageData = context.createImageData(canvas.width, canvas.height);
     
     var torus = gen_torus(canvas.width, canvas.height, i, j, l);
     
     for (var location = 0; location < torus.length; location++)
-	imageData.data.set([255, 255, 255, 225], torus[location]);
+	imageData.data.set([0, 0, 0, 225], torus[location]);
 
     j += 0;
-    l += 0.003;
+    l += 0.005;
     
     context.putImageData(imageData, 0,0);
 }
-
-function stop_animation(event) {
-    if (event.deltaY > 0) {
-	clearInterval(tick);
-	document.body.style.backgroundColor="white";
-    }
-}
-
-const el = document.body;
-el.onwheel = stop_animation;
